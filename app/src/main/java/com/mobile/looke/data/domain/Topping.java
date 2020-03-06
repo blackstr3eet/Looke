616a95b2cd01ee60a28bb9ae09b9bf83aaaeb20d
Marcos Walker Silva Correia <marquinhos_thuglife@hotmail.com>
2020-03-06 18:17:45 -0300

    remove import unused in class

--- app/src/main/java/com/mobile/looke/data/domain/Topping.java
+++ app/src/main/java/com/mobile/looke/data/domain/Topping.java
@@ -10,8 +10,6 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
-import com.google.common.base.Objects;
-
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @JsonPropertyOrder({
         "id",
