e027ed318f7e22d404509f917582b48abaf9f75b
Marcos Walker Silva Correia <marquinhos_thuglife@hotmail.com>
2020-03-06 02:19:16 -0300

    remove toolbar and include action bar

---
+++ app/src/main/java/com/mobile/looke/ToppingActivity.java
@@ -0,0 +1,22 @@
+package com.mobile.looke;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ToppingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle(getString(R.string.toolbar_title_toppings));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
+    }
+}
